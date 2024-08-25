a559363758a03b5edacef37062414809ceb2a14d
Augustinejude <divinefavour4jude@gmail.com>
2024-08-25 16:58:50 -0700

    Updated database URI and added environment-specific configurations to config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'c1ea1a224d78e8555f3d46116db503db')
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL', 'sqlite:///data.db')
+        'DATABASE_URL', 'sqlite:///instance/taskmaster.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
