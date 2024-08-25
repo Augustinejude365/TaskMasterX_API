746fe9687373b50b202eeb37e81e51ab6f2cea01
Augustinejude <divinefavour4jude@gmail.com>
2024-08-25 15:10:11 -0700

    Updated requirements.tx with necessary Python packages and new dependencies

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'c1ea1a224d78e8555f3d46116db503db')
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL', 'sqlite:///data.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
